4a1cf8f62ba84264cda53ac5d60821ac1444c60f
alaixy <alexismortier@msn.com>
2017-03-13 13:55:34 +0100

    13.55

---
+++ index.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="web/css/bootstrap.css"><!--css bootstrap-->
+<link rel="stylesheet" href="web/css/style.css"><!--notre style-->
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//layout.php TOUJOURS a la fin
+include_once('layouts/layout.php');
+?>
---
+++ layouts/layout.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>IMIE | Ecole de la filière du numérique</title>
+</head>
+<body>
+	<div class="container">
+		<div class="header">
+			<div class="row">
+				<div class="col-lg-12">
+					<img src="web/img/logo_imie.png" alt="logo imie"/>
+				</div>
+				<div class="col-lg-12">
+					<nav class="menu">
+						<ul>
+							<li><a href="">Accueil</a></li>
+							<li><a href=""></a>Formations</li>
+								<div class="formation">
+									<div class="alternance">
+										<ul>
+											<li><a href="">IT Start (Bac +1)</a></li>
+											<li><a href="">Digital Factory (BTS Support en Informatique (Bac +2)</a></li>
+											<li><a href="">Administrateur Systeme et Réseaux (Bac +3)</a></li>
+											<li><a href="">Développeur en Projets Numériques (Bac +3)</a></li>
+											<li><a href="">Concepteur Développeur en Projets Numériques (Bac +3)</a></li>
+											<li><a href="">Ingéniere et Management des Systèmes d'information (Bac +4/+5)</a></li>
+										</ul>
+									</div>
+									<div class="continu">
+										<ul>
+											<li><a href="">BTS SIO (Bac +2)</a></li>
+											<li><a href="">Bachelor PRODIGITAL (Bac +3)</a></li>
+										</ul>
+									</div>
+									<a href="">L'incubateur de Start-Up</a>
+									<a href="">Les formation pour entreprise</a>
+								</div>
+								
+							<li><a href=""></a>Calendrier</li>
+							<li><a href=""></a>Candidater</li>
+							<li><a href=""></a>Contact</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
