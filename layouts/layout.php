dfc4e2f66fe67c68e3c081679d018c14e2c9dee0
alaixy <alexismortier@msn.com>
2017-03-13 17:03:09 +0100

    17.03

--- layouts/layout.php
+++ layouts/layout.php
@@ -12,34 +12,13 @@
 					<img src="web/img/logo_imie.png" alt="logo imie"/>
 				</div>
 				<div class="col-lg-12">
-					<nav class="menu">
+					<nav>
 						<ul>
-							<li><a href="">Accueil</a></li>
-							<li><a href=""></a>Formations</li>
-								<div class="formation">
-									<div class="alternance">
-										<ul>
-											<li><a href="">IT Start (Bac +1)</a></li>
-											<li><a href="">Digital Factory (BTS Support en Informatique (Bac +2)</a></li>
-											<li><a href="">Administrateur Systeme et Réseaux (Bac +3)</a></li>
-											<li><a href="">Développeur en Projets Numériques (Bac +3)</a></li>
-											<li><a href="">Concepteur Développeur en Projets Numériques (Bac +3)</a></li>
-											<li><a href="">Ingéniere et Management des Systèmes d'information (Bac +4/+5)</a></li>
-										</ul>
-									</div>
-									<div class="continu">
-										<ul>
-											<li><a href="">BTS SIO (Bac +2)</a></li>
-											<li><a href="">Bachelor PRODIGITAL (Bac +3)</a></li>
-										</ul>
-									</div>
-									<a href="">L'incubateur de Start-Up</a>
-									<a href="">Les formation pour entreprise</a>
-								</div>
-								
-							<li><a href=""></a>Calendrier</li>
-							<li><a href=""></a>Candidater</li>
-							<li><a href=""></a>Contact</li>
+							<li onmouseover="this.style.background='#48B9C7';" onmouseout="this.style.background='';this.style.color='';"><a href="">Accueil</a></li>
+							<li onmouseover="this.style.background='#E5324B';" onmouseout="this.style.background='';this.style.color='';"><a href="">Formations</a></li>
+							<li onmouseover="this.style.background='#FDB515';" onmouseout="this.style.background='';this.style.color='';"><a href="">Calendrier</a></li>
+							<li onmouseover="this.style.background='#48B9C7';" onmouseout="this.style.background='';this.style.color='';"><a href="">Candidater</a></li>
+							<li onmouseover="this.style.background='#E5324B';" onmouseout="this.style.background='';this.style.color='';"><a href="">Contact</a></li>
 						</ul>
 					</nav>
 				</div>
