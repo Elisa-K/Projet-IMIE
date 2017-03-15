3c4dbfc010dd963c4705321bc68504131e0ee30b
alaixy <alexismortier@msn.com>
2017-03-15 11:29:44 +0100

    11.29

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <link rel="stylesheet" href="web/css/bootstrap.css"><!--css bootstrap-->
 <link rel="stylesheet" href="web/css/style.css"><!--notre style-->
 <?php
+include_once('library/PDOFactory.php');
+
+
+$pdo = PDOFactory::getMysqlConnection();
+
+if (isset($_REQUEST['action'])) {
+	$action = $_REQUEST['action'];
+} else {
+	$action = null;
+}
+
+switch ($action) {
+
+	default:
+			$vueAAfficher = "views/accueil.php";
+	break;
+	}
+
 
 
 
---
+++ views/accueil.php
@@ -0,0 +1,30 @@
+<div class="row">
+	<div class="container">
+		<div class="col-sm-12 col-md col-lg-6">
+			<a href=""><img class="imgHome" src="https://imie-ecole-informatique.fr/sites/default/files/imce/jpo-25-mars-site.png" alt="portes ouvertes"/></a>
+		</div>
+		<div class="col-sm-12 col-md col-lg-6 img2">
+			<a href=""><span class="typo-intro">
+				<span class="gray">Decouvrez<br>les</span>
+				<span class="blue">cursus</span>
+				<span class="gray">et</span>
+				<span class="gray">les</span>
+				<span class="yellow">formations</span>
+				<span class="gray">de l'ecole.</span>
+			</span></a>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="container">
+		<div class="col-lg-3">
+			<a href="">Facebook</a>
+		</div>
+		<div class="col-lg-3">
+			<a href="">Twitter</a>
+		</div>
+		<div class="col-lg-6">
+		
+		</div>
+	</div>
+</div>
