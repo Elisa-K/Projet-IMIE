57e95f6c854e78f9ecf62f4ab990a0a99f2b98b3
alaixy <alexismortier@msn.com>
2017-03-24 09:43:32 +0100

    09.43

--- admin/views/listContact.php
+++ admin/views/listContact.php
@@ -11,7 +11,7 @@
 <form  action="./dashboard.php" method="post">
 <input type="hidden" name="action">
 
-    <input type="submit" value="Tout sélectionner" class="btn btn-default btn-round">
+    <input type="submit" value="select" id="selectAll" class="btn btn-default btn-round" onclick="do_this()">
 
     <input type="submit" value="Supprimer" onclick="this.form.action.value='deleteContact';this.form.submit();" class="btn btn-default btn-round">
 
